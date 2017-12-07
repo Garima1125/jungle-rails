d327914871a248ee71916d57858cf9d1e5feba1f
garima1125 <garima.arya1101@gmail.com>
2017-12-07 20:13:46 +0000

    implemented login,logout and signup functions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      @current_user = session[:user_id]
+      redirect_to root_url, notice: "You're Logged in!"
+    else
+      redirect_to login_url, notice:"Email or password is invalid"
+    end
+  end
+
+  def destroy
+   session[:user_id] = nil
+   redirect_to root_url, notice: 'Thank you for shopping with us!!'
+  end
+
+  def session_params
+    params.require(:session).permit(
+      :email,
+      :password
+    )
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,15 @@ Rails.application.routes.draw do
     resources :categories, only: [:new, :index, :create]
   end
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # resources :sessions
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
